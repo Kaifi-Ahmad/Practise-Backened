d01a25bccb1bcf14f14c92c65095b6686b3b1309
Kaif Ahmad <kaifmughal113@gmail.com>
2026-06-22 16:51:18 +0500

    fix the little more code

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,6 @@
 import mongoose,{Schema} from 'mongoose'
 
-const subscriptionSchema=new Schema({
+const subscribedSchema=new Schema({
     subscriber:{
         type:Schema.Types.ObjectId,
         ref:"User"
@@ -13,4 +13,4 @@ const subscriptionSchema=new Schema({
     timestamps:true
 })
 
-export const Subscription=mongoose.model("Subscription",subscriptionSchema)
+export const Subscription=mongoose.model("Subscription",subscribedSchema)
